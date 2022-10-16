7e8667371ff331e88f2d713c9e2462737b21704a
plinio <p.grijalbar@uniandes.edu.co>
2022-10-16 13:52:44 -0500

    login and register

--- vistas/VistaLogin.py
+++ vistas/VistaLogin.py
@@ -9,4 +9,11 @@ usuario_schema = UsuarioSchema()
 
 class VistaLogIn(Resource):
     def post(self):
-        return False
+        usuario = Usuario.query.filter(Usuario.username == request.form["username"],
+                                       Usuario.password == request.form["password"]).first()
+        db.session.commit()
+        if usuario is None:
+            return {"ok": False, "mensaje": "El usuario no existe"}, 404
+        else:
+            token_de_acceso = create_access_token(identity=usuario.id, additional_claims=usuario_schema.dump(usuario))
+            return {"ok": True, "mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
