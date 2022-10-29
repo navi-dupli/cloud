866d01df7b7d6022c31b74725649051b5d2595bd
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2022-10-29 04:58:40 +0200

    se ajusta para envio de correo

--- worker/build.py
+++ worker/build.py
@@ -13,6 +13,11 @@ def create_app(config_name):
     app.config['PROPAGATE_EXCEPTIONS'] = True
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
     app.config['JWT_SECRET_KEY'] = 'frase-secreta-para-cifrar-el-token'
-
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'cloudteam35@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'uaeyqqzjlpvumqge'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
 
     return app
