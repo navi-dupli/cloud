8a3b916c77734f1f269a57be691cd30b6e08d437
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2022-11-09 04:25:01 +0100

    se ajusta codigo

--- api-rest/vistas/VistaFiles.py
+++ api-rest/vistas/VistaFiles.py
@@ -2,14 +2,13 @@ import io
 import logging
 import os
 
-from flask import  send_file
+from flask import send_file
 from flask_jwt_extended import jwt_required
 
 from env import CONVERTED_FOLDER, UPLOAD_FOLDER
 from modelos import Task, db, UsuarioSchema, TaskSchema, TaskStatus
 from flask_restful import Resource, abort
 from utils import StorageUtils
-
 
 
 def read_file(file_path, file_name):
@@ -21,14 +20,17 @@ def read_file(file_path, file_name):
     )
 
 
-def get_file(task):
+def get_file(task,request_file):
     try:
-        if task.estado == TaskStatus.PROCESSED:
+        if request_file == 'original':
             file_path = os.path.join(f'{CONVERTED_FOLDER}', f'{task.id}.{task.new_format.lower()}')
             return read_file(file_path, task.new_file)
-        elif task.estado == TaskStatus.UPLOADED:
-            file_path = os.path.join(f'{UPLOAD_FOLDER}', f'{task.id}.{task.format.lower()}')
-            return read_file(file_path, task.file)
+        elif request_file == 'converted':
+            if task.estado == TaskStatus.UPLOADED:
+                file_path = os.path.join(f'{UPLOAD_FOLDER}', f'{task.id}.{task.format.lower()}')
+                return read_file(file_path, task.file)
+            else:
+                abort(404)
         else:
             abort(404)
     except FileNotFoundError:
@@ -40,10 +42,15 @@ class VistaFiles(Resource):
     def get(self, filename):
         logging.info(f'DOWNLOAD: file--> {filename}')
         task = db.session.query(Task).filter(Task.file == filename).first()
+        request_file = 'original'
         if task:
             '''Archivo sin convertir'''
         else:
             task = db.session.query(Task).filter(Task.new_file == filename).first()
+            request_file = 'converted'
             '''Archivo convertido'''
 
-        return get_file(task)
+        if task:
+            return get_file(task, request_file)
+        else:
+            return {},404
