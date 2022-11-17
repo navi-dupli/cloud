276433f584b93a621b65147c06992434803ad734
plinio <p.grijalbar@uniandes.edu.co>
2022-11-17 00:37:21 -0500

    subscriber pub/sub endpoint

--- worker/app.py
+++ worker/app.py
@@ -1,9 +1,8 @@
 import datetime
 import io
 import logging
-import os
 import traceback
-from google.cloud import pubsub_v1
+import base64
 import json
 
 from pydub import AudioSegment
@@ -13,6 +12,7 @@ from env import UPLOAD_FOLDER, CONVERTED_FOLDER, MAIL_NOTIFICATION_ENABLED, SUBS
 from modelos import db, Task, TaskStatus, Usuario, TaskSchema
 from flask_mail import Mail
 from flask_mail import Message
+from flask import request
 from utils import StorageUtils
 
 task_schema = TaskSchema()
@@ -80,36 +80,10 @@ def encolar(id_task):
                     logging.error(traceback.format_exc())
                 print("Finalizo conversión", datetime.datetime.now(), task.new_file)
 
-
-project_id = PROJECT_ID
-subscription_id = SUBSCRIBER
-# Number of seconds the subscriber should listen for messages
-# timeout = 5.0
-
-subscriber = pubsub_v1.SubscriberClient()
-# The `subscription_path` method creates a fully qualified identifier
-# in the form `projects/{project_id}/subscriptions/{subscription_id}`
-subscription_path = subscriber.subscription_path(project_id, subscription_id)
-
-
-def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    dataDecode = message.data.decode("utf-8")
-    data = json.loads(dataDecode)
-    print(data["id"])
-    encolar(data["id"])
-    message.ack()
-
-
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-print(f"Listening for messages on {subscription_path}..\n")
-
-# # Wrap subscriber in a 'with' block to automatically call close() when done.
-# with subscriber:
-#     try:
-#         # When `timeout` is not set, result() will block indefinitely,
-#         # unless an exception is encountered first.
-#         streaming_pull_future.result(timeout=timeout)
-#     except TimeoutError:
-#         print("Final -------->")
-#         streaming_pull_future.cancel()  # Trigger the shutdown.
-#         streaming_pull_future.result()  # Block until the shutdown is complete.
+@app.route('/receive_messages', methods=['POST'])
+def receive_messages_handler():
+    envelope = json.loads(request.data.decode('utf-8'))
+    payload = json.loads(base64.b64decode(envelope['message']['data']))
+    encolar(payload["id"])
+    # Returning any 2xx status indicates successful receipt of the message.
+    return 'OK', 200
